acee5f6deaedf2a41a98d0661b1dce5583f835fe
Oix135 <oix@mail.ru>
2024-02-02 20:32:04 +0300

    Добавьте файлы проекта.

---
+++ Exam14/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Exam14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var work = new Work();
+
+            int countRecords = 2;
+
+            while (true)
+            {
+                var input = Console.ReadKey().KeyChar;
+
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+                if (!parsed || pageNumber < 1 || pageNumber > 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                else
+                {
+                    var pageContent = work.GetContacts(pageNumber, countRecords);
+                    Console.WriteLine();
+
+                    // выводим результат
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber + " e-mail: " + entry.Email);
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Exam14/Work.cs
@@ -0,0 +1,49 @@
+﻿namespace Exam14
+{
+    internal class Work
+    {
+        private List<Contact> phoneBook;
+        public Work()
+        {
+            SetData();
+        }
+
+        private void SetData()
+        {
+            phoneBook =
+            [
+                // добавляем контакты
+                new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"),
+                new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"),
+                new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"),
+                new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"),
+                new Contact("Сергей", "Брин", 799900000013, "serg@google.com"),
+                new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"),
+                new Contact("Ларри", "Пейдж", 799900000013, "larry@google.com"),
+            ];
+
+        }
+
+        internal IEnumerable<Contact> GetContacts(int pageNumber, int countRecords)
+        {
+            var pageContent = phoneBook.OrderBy(a=>a.Name).ThenBy(a=>a.LastName).Skip((pageNumber - 1) * 2).Take(countRecords);
+            return pageContent;
+        }
+        
+    }
+    internal class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public string Name { get; }
+        public string LastName { get; }
+        public long PhoneNumber { get; }
+        public string Email { get; }
+    }
+}
